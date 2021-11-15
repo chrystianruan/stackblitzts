eb961a3a6a684c558a35eb023f5bc57696252fcd
chrystianruan <chrystianr37@gmail.com>
2021-11-15 09:54:51 -0300

    1 file modified

---
+++ index.ts
@@ -0,0 +1,9 @@
+const m1 = +prompt('Módulo 1 (entre 0 e 100): ');
+const m2 = +prompt('Módulo 2 (entre 0 e 100): ');
+
+if (m1 >= 0 && m1 <= 100 && m1 >= 0 && m1 <= 100) {
+  let media = (m1 * 4 + m2 * 6) / 10;
+  document.write('Média: ' + media);
+} else {
+  document.write('Algumas das notas não estão entre 0 e 100.');
+}
