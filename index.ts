f563887ac82c765b427dc550860e40f56b3d31be
chrystianruan <chrystianr37@gmail.com>
2021-11-16 12:11:06 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const m1 = +prompt('Módulo 1 (entre 0 e 100): ');
 const m2 = +prompt('Módulo 2 (entre 0 e 100): ');
 
-if (m1 >= 0 && m1 <= 100 && m1 >= 0 && m1 <= 100) {
+if (m1 >= 0 && m1 <= 100 && m2 >= 0 && m2 <= 100) {
   let media = (m1 * 4 + m2 * 6) / 10;
   document.write('Média: ' + media);
 } else {
